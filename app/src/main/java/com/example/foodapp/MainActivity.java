9f4bc97e59e6fd5396b2582b47dc878c5b202f9d
PatrykBichta <PatrykBichta@gmail.com>
2023-05-18 23:05:39 +0200

    Nowy projekt v1
    -ekran tytułowy z przejsciem
     po 3 sekundach
    
    -druga aktywnosc

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Twój kod do przejścia do drugiej aktywności
+                Intent intent = new Intent(MainActivity.this, DrugaAktywnosc.class);
+                startActivity(intent);
+                finish(); // Opcjonalnie, aby zamknąć pierwszą aktywność po przejściu do drugiej
+            }
+        }, 3000); // 3000 milisekund = 3 sekundy
     }
 }
