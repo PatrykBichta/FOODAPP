c3268cc392e0d0cfffdbc6f6eedc9c366cd54c44
PatrykBichta <PatrykBichta@gmail.com>
2023-05-18 23:37:13 +0200

    Nowy projekt v1
    -ekran tytułowy z przejsciem
     po 3 sekundach
    
    -druga aktywnosc
    obrazki i napisy

--- app/src/main/java/com/example/foodapp/DrugaAktywnosc.java
+++ app/src/main/java/com/example/foodapp/DrugaAktywnosc.java
@@ -2,13 +2,41 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DrugaAktywnosc extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_druga_aktywnosc);
+
+
+
+        Button button = findViewById(R.id.buttonAbout); // Zastąp "myButton" swoim id przycisku
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Twój kod do uruchomienia kolejnej aktywności
+                Intent intent = new Intent(DrugaAktywnosc.this, AboutAktywnosc.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
     }
+
+
 }
