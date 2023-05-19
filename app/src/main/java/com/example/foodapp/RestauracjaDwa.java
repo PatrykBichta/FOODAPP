9ce192c421267e3ed57219764745b2ba2a0ac8c4
PatrykBichta <PatrykBichta@gmail.com>
2023-05-19 10:33:56 +0200

    Nowy projekt v1
    -ekran tytułowy z przejsciem
     po 3 sekundach
    
    -druga aktywnosc
    obrazki i napisy
    
    -aktywnosc about
    imiona i nazwiska autorow
    przycisk powrotu do akt2

--- app/src/main/java/com/example/foodapp/RestauracjaDwa.java
+++ app/src/main/java/com/example/foodapp/RestauracjaDwa.java
@@ -2,7 +2,10 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RestauracjaDwa extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class RestauracjaDwa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restauracja_dwa);
+
+
+        ////////////////////KOD KOSZYKA
+
+
+
+
+
+
+
+
+
+
+
+        //////////////////////PRZYCISK WROC
+        Button button = findViewById(R.id.buttonWROC3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RestauracjaDwa.this, DrugaAktywnosc.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/foodapp/RestauracjaJeden.java
+++ app/src/main/java/com/example/foodapp/RestauracjaJeden.java
@@ -2,7 +2,10 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RestauracjaJeden extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class RestauracjaJeden extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restauracja_jeden);
+
+
+
+
+
+
+
+
+        Button button = findViewById(R.id.buttonWROC2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RestauracjaJeden.this, DrugaAktywnosc.class);
+                startActivity(intent);
+            }
+        });
     }
 }
